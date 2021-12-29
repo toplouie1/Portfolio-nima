db42947e18eefcd22d6d10d93bcd395ae3d43b7c
Nima sherpa <Toplouie78@gmail.com>
2021-12-29 18:26:11 -0500

    nav almost complete

--- portfolionima/src/App.js
+++ portfolionima/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Nav />
+		</div>
+	);
 }
 
 export default App;
--- portfolionima/src/components/Nav.js
+++ portfolionima/src/components/Nav.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
-	return <div></div>;
+	return (
+		<div id="navContainer">
+			<div id="mainNav">
+				<div className="logo">N</div>
+				<ul className="allList">
+					<li>About</li>
+					<li>Projects</li>
+					<li>Contact</li>
+					<button>Resume</button>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default Nav;
