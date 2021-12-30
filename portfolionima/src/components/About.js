41e566fa75e538ff929b1440c5dfa4a90084289d
Nima sherpa <Toplouie78@gmail.com>
2021-12-30 16:25:18 -0500

    addint the img to the page

--- portfolionima/src/components/About.js
+++ portfolionima/src/components/About.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./About.css";
 import html from "../skillsimg/html.png";
+import css from "../skillsimg/css.png";
+import js from "../skillsimg/js.png";
+import boot from "../skillsimg/boot.png";
+import npm from "../skillsimg/npm.png";
+import react from "../skillsimg/react.png";
+import sql from "../skillsimg/sql.png";
+import node from "../skillsimg/node.png";
+import express from "../skillsimg/express.png";
+import api from "../skillsimg/apii.png";
+import git from "../skillsimg/git.png";
+import vscode from "../skillsimg/vscode.png";
 
 function About() {
 	return (
@@ -28,12 +39,23 @@ function About() {
 			<p>My goal is to be an efficient coder by the end of the pursuit days.</p>
 			<h3> Skills </h3>
 
-			<div>
+			<div className="skills">
 				{" "}
 				<img src={html} alt="html" />
+				<img src={css} alt="css" />
+				<img src={js} alt="js" />
+				<img src={react} alt="react" />
+				<img src={boot} alt="boot" />
+				<img src={git} alt="git" />
+				<img src={express} alt="express" />
+				<img src={node} alt="node" />
+				<img src={sql} alt="sql" />
+				<img src={api} alt="api" />
+				<img src={npm} alt="npm" />
+				<img src={vscode} alt="vscode" />
 			</div>
 
-			<p className="skills">
+			<p className="skillss">
 				HTML/CSS, JavaScript, Node, Express, React/Redux, PostgreSQL, APIs, Git,
 				Data Structures,
 			</p>
