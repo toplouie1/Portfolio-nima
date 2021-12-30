a7e917982666c3dce632d111d3e8df33a67c48f4
Nima sherpa <Toplouie78@gmail.com>
2021-12-29 23:41:17 -0500

    adding boxshadow.hover

--- portfolionima/src/components/Projects.js
+++ portfolionima/src/components/Projects.js
@@ -8,7 +8,7 @@ import pixel from "../images/pixelPainter.png";
 function Projects() {
 	return (
 		<div id="projectsContainer">
-			<h1 className="headingProjects">Projects</h1>
+			<h1 className="headingProjects">Projécts</h1>
 			<div className="project">
 				<div>
 					<a href="https://youtube-clonana.netlify.app/" target="blank">
